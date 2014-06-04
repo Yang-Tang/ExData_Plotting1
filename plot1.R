7ab5e50a5375a4eaa40d8f59ac981e962d21ec90
doubletang <doubletang@gmail.com>
2014-06-04 22:45:30 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,13 +1,35 @@
 url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 download.file(url, 'cp1data.zip')
 unzip('cp1data.zip')
-data <- read.table('household_power_consumption.txt', head = T, sep = ";", na.strings= '?', nrows = 1000, stringsAsFactors = F)
-data <- read.table('household_power_consumption.txt', head = T, sep = ";", na.strings= '?', stringsAsFactors = F)
-data$dt <- as.POSIXlt(paste(data[, 1], data[, 2]), format = '%d/%m/%Y %H:%M:%S')
-data1 <- data[data$Date =='2/2/2007' | data$Date =='1/2/2007',]
+#data <- read.table('household_power_consumption.txt', head = T, sep = ";", na.strings= '?', nrows = 1000, stringsAsFactors = F)
+#data <- read.table('household_power_consumption.txt', head = T, sep = ";", na.strings= '?', stringsAsFactors = F)
+#data$dt <- as.POSIXlt(paste(data[, 1], data[, 2]), format = '%d/%m/%Y %H:%M:%S')
+#data1 <- data[data$Date =='2/2/2007' | data$Date =='1/2/2007',]
+con <- file('household_power_consumption.txt', open = 'r')
+n <- 0
+start <- 0
+end <- 0
+find <- F
+name <- strsplit(readLines(con, 1), ';')[[1]]
+while(length(line <- readLines(con, 1)) > 0){
+  n = n + 1
+  if(grepl('^1/2/2007', line)){
+    if(!find){
+      start <- n
+      find <- T
+    }
+  }
+  if(grepl('^3/2/2007', line)){
+    if(find){
+      end <- n - 1
+      break()
+    }
+  }
+}
+close(con)
+data <- read.table('household_power_consumption.txt', head = F, sep = ";", na.strings= '?', stringsAsFactors = F, skip = start, nrow = end - start + 1)
+names(data) <- name
 
-summary(data1$Global_active_power)
+png('plot1.png', 480, 480)
 hist(data1$Global_active_power, main='Global Active Power', xlab = 'Global Active Power(kilowatts)', col = 'red')
-dev.copy(png, file='plot1.png')
-dev.off
-dev.off(4)
+dev.off()
