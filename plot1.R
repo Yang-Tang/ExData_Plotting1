64bfcb81574b362815314940f9a20fdb11278786
doubletang <doubletang@gmail.com>
2014-06-05 18:22:30 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,7 @@
 url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 download.file(url, 'cp1data.zip')
 unzip('cp1data.zip')
-#data <- read.table('household_power_consumption.txt', head = T, sep = ";", na.strings= '?', nrows = 1000, stringsAsFactors = F)
-#data <- read.table('household_power_consumption.txt', head = T, sep = ";", na.strings= '?', stringsAsFactors = F)
-#data$dt <- as.POSIXlt(paste(data[, 1], data[, 2]), format = '%d/%m/%Y %H:%M:%S')
-#data1 <- data[data$Date =='2/2/2007' | data$Date =='1/2/2007',]
+
 con <- file('household_power_consumption.txt', open = 'r')
 n <- 0
 start <- 0
